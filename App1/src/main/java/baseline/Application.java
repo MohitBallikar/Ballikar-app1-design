5d33283e2fbb2cb686437dd8a0c880092bd7b737
MohitBallikar <mohitballikar@gmail.com>
2021-10-22 19:03:45 -0400

    starter pseudo, will add some driver startup code and more detasils

---
+++ App1/src/main/java/baseline/Application.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 1 Solution
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+public class Application {
+}
+
+
+/*
+Call helper functions
+Define the list of 256 unique items
+Title of List must be at least 3 characters long
+Need to allow for the user to expand and create a new to do list
+Edits need to be possible
+Filtration of the list must also be there
+ */
