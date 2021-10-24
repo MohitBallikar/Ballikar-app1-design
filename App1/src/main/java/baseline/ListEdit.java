7dc369b7b41fa5d614aec0b9136124e71c247f9d
MohitBallikar <mohitballikar@gmail.com>
2021-10-24 19:25:34 -0400

    fixed gitignore and some other troubleshooting

---
+++ App1/src/main/java/baseline/ListEdit.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 1 Solution
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+public class ListEdit {
+
+
+
+}
+
+
+/*
+Parse through the list and provide values
+Add/subtract values from the list
+The
+ */
---
+++ App1/src/test/java/baseline/ListEditTest.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 1 Solution
+ *  Copyright 2021 Mohit Ballikar
+ */
+package baseline;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListEditTest {
+/*
+Test for correct title length
+Test
+ */
+}
